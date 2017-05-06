c78a380d1657a8661380c2f53814d6f93fc603bf
brianping7 <brianping7@gmail.com>
2017-05-06 10:48:58 +0800

    update subdevice find logic

--- plugin/miHome/index.js
+++ plugin/miHome/index.js
@@ -135,7 +135,7 @@ class MiHome {
     if(this.device.type == 'gateway') {
       that.device.on('deviceAvailable', subDevice => {
         if (subDevice.type == 'power-plug') {
-            subDevice.setPower(1, false)
+            subDevice.setPower(0, false)
             .then(power => console.log(subDevice.type + " Power " + power))
             .catch(err => console.error(subDevice.type + " Failed :" + err));
         }
